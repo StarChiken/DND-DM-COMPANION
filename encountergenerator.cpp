fa1bf1e120a8064a1c9c81bc099bdcbb1974239e
Myth <mythalvarado@gmail.com>
2024-04-14 19:36:11 -0800

    update again

--- encountergenerator.cpp
+++ encountergenerator.cpp
@@ -100,7 +100,7 @@ int main() {
 
     // Gets user inputs
     int desiredCR, enemyCount;
-    cout << "Enter the desired challenge rating for the enemies (1-10): ";
+    cout << "Enter the desired max challenge rating for the enemies (1-10): ";
     cin >> desiredCR;
     cout << "Enter the desired enemy count: ";
     cin >> enemyCount;
